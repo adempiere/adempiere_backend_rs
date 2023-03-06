43a316d9de982608ab212b0cd25f8dea03fccd06
yamelsenih <ysenih@erpya.com>
2023-03-06 12:04:41 -0400

    Add support to: - Process - Create Entity - Update Entity - Delete entity

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,7 @@
 use std::env;
-use adempiere_backend_rs::middleware::{KeyValue, CreateEntityRequest};
+use adempiere_backend_rs::middleware::{KeyValue, CreateEntityRequest, UpdateEntityRequest, DeleteEntityRequest, RunBusinessProcessRequest};
 use adempiere_backend_rs::middleware::middleware_service_client::MiddlewareServiceClient;
-use adempiere_backend_rs::models::documents::{EntityNewDocument, EntityResponse};
+use adempiere_backend_rs::models::documents::{EntityNewDocument, EntityResponse, EntityUpdateDocument, EntityDeleteDocument, RunProcessDocument, ProcessResponse};
 use dotenv::dotenv;
 use local_ip_address::local_ip;
 use salvo::prelude::*;
@@ -39,7 +39,12 @@ async fn main() {
         .push(
             Router::with_path("entities")
                 .post(create_entity)
-                // .patch(update_device)
+                .patch(update_entity)
+                .delete(delete_entity)
+        )
+        .push(
+            Router::with_path("process")
+                .post(run_process)
         )
         ;
     log::info!("{:#?}", router);
@@ -66,6 +71,8 @@ async fn create_entity<'a>(_req: &mut salvo::Request, _document: EntityNewDocume
     }
     let token_value = token_value.unwrap();
     let attributes: Vec<KeyValue> = _entity.attributes.unwrap().iter().map(|value| value.to_owned().to_grpc_value()).collect();
+    let middleware_host = env::var("MIDDLEWARE_HOST");
+    let middleware_host = middleware_host.unwrap();
     let channel = Channel::from_static("http://[::1]:50059").connect().await.unwrap();
     let token: MetadataValue<_> =  token_value.parse().unwrap();
     let mut client = MiddlewareServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
@@ -80,6 +87,134 @@ async fn create_entity<'a>(_req: &mut salvo::Request, _document: EntityNewDocume
             let entity = response.get_ref();
             log::info!("{}", entity.to_owned().id);
             _res.render(Json(EntityResponse::from_entity(entity.to_owned())));
+        }, 
+        Err(error) => {
+            log::warn!("{}", error);
+            _res.set_status_error(StatusError::internal_server_error());
+            _res.render(Json(format!("{}", error.message())));
+        }
+    }
+}
+
+#[handler]
+async fn update_entity<'a>(_req: &mut salvo::Request, _document: EntityUpdateDocument, _res: &mut Response) {
+    let _entity = _document.entity;
+    if _entity.is_none() {
+       return _res.render("Entity Is Mandatory");
+    }
+    let _entity = _entity.unwrap();
+    if _entity.table_name.is_none() {
+        return _res.render("Table Is Mandatory");
+    }
+    if _entity.attributes.is_none() {
+        return _res.render("Attributes are Mandatory");
+    }
+    if _entity.id.is_none() {
+        return _res.render("ID is Mandatory");
+    }
+    let token_value = _req.header::<String>("authorization");
+    if token_value.is_none() {
+        return _res.set_status_code(StatusCode::FORBIDDEN);
+    }
+    let token_value = token_value.unwrap();
+    let attributes: Vec<KeyValue> = _entity.attributes.unwrap().iter().map(|value| value.to_owned().to_grpc_value()).collect();
+    let channel = Channel::from_static("http://[::1]:50059").connect().await.unwrap();
+    let token: MetadataValue<_> =  token_value.parse().unwrap();
+    let mut client = MiddlewareServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
+    });
+    match client.update_entity(tonic::Request::new(UpdateEntityRequest {
+        table_name: _entity.table_name.unwrap(),
+        id: _entity.id.unwrap(),
+        attributes: attributes
+    })).await {
+        Ok(response) => {
+            let entity = response.get_ref();
+            log::info!("{}", entity.to_owned().id);
+            _res.render(Json(EntityResponse::from_entity(entity.to_owned())));
+        }, 
+        Err(error) => {
+            log::warn!("{}", error);
+            _res.set_status_error(StatusError::internal_server_error());
+            _res.render(Json(format!("{}", error.message())));
+        }
+    }
+}
+
+#[handler]
+async fn delete_entity<'a>(_req: &mut salvo::Request, _document: EntityDeleteDocument, _res: &mut Response) {
+    let _entity = _document.entity;
+    if _entity.is_none() {
+       return _res.render("Entity Is Mandatory");
+    }
+    let _entity = _entity.unwrap();
+    if _entity.table_name.is_none() {
+        return _res.render("Table Is Mandatory");
+    }
+    if _entity.id.is_none() {
+        return _res.render("ID is Mandatory");
+    }
+    let token_value = _req.header::<String>("authorization");
+    if token_value.is_none() {
+        return _res.set_status_code(StatusCode::FORBIDDEN);
+    }
+    let token_value = token_value.unwrap();
+    let channel = Channel::from_static("http://[::1]:50059").connect().await.unwrap();
+    let token: MetadataValue<_> =  token_value.parse().unwrap();
+    let mut client = MiddlewareServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
+    });
+    match client.delete_entity(tonic::Request::new(DeleteEntityRequest {
+        table_name: _entity.table_name.unwrap(),
+        id: _entity.id.unwrap()
+    })).await {
+        Ok(_) => {
+            log::info!("{}", _entity.id.unwrap());
+            _res.render(Json("Ok"));
+        }, 
+        Err(error) => {
+            log::warn!("{}", error);
+            _res.set_status_error(StatusError::internal_server_error());
+            _res.render(Json(format!("{}", error.message())));
+        }
+    }
+}
+#[handler]
+async fn run_process<'a>(_req: &mut salvo::Request, _document: RunProcessDocument, _res: &mut Response) {
+    let _process = _document.process;
+    if _process.is_none() {
+       return _res.render("Process Is Mandatory");
+    }
+    let _process = _process.unwrap();
+    if _process.process_code.is_none() {
+        return _res.render("Process Code is Mandatory");
+    }
+    let token_value = _req.header::<String>("authorization");
+    if token_value.is_none() {
+        return _res.set_status_code(StatusCode::FORBIDDEN);
+    }
+    let token_value = token_value.unwrap();
+    let parameters: Vec<KeyValue> = _process.parameters.unwrap_or_default().iter().map(|value| value.to_owned().to_grpc_value()).collect();
+    let channel = Channel::from_static("http://[::1]:50059").connect().await.unwrap();
+    let token: MetadataValue<_> =  token_value.parse().unwrap();
+    let mut client = MiddlewareServiceClient::with_interceptor(channel, move |mut req: Request<()>| {
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
+    });
+    match client.run_business_process(tonic::Request::new(RunBusinessProcessRequest {
+        process_code: _process.process_code.unwrap(),
+        table_name: _process.table_name.unwrap_or_default(),
+        id: _process.id.unwrap_or_default(),
+        table_selected_id: _process.table_selected_id.unwrap_or_default(),
+        parameters: parameters,
+        ..Default::default()
+    })).await {
+        Ok(response) => {
+            let process_respose = response.get_ref();
+            log::info!("{:?}", process_respose.to_owned());
+            _res.render(Json(ProcessResponse::from_process_response(process_respose.to_owned())));
         }, 
         Err(error) => {
             log::warn!("{}", error);
--- src/models/documents.rs
+++ src/models/documents.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use salvo::prelude::*;
 
-use crate::middleware::{KeyValue, Value, ValueType, Entity};
+use crate::middleware::{KeyValue, Value, ValueType, Entity, Decimal, RunBusinessProcessResponse};
 
 #[derive(Serialize, Debug, Clone)]
 pub struct EntityResponse {
@@ -27,12 +27,63 @@ impl EntityResponse {
     }
 }
 
+#[derive(Serialize, Debug, Clone)]
+pub struct ProcessLog {
+    pub record_id: Option<i32>,
+    pub log: Option<String>
+}
+
+#[derive(Serialize, Debug, Clone)]
+pub struct ProcessResponse {
+    pub id: Option<i32>,
+    pub is_error: Option<bool>,
+    pub summary: Option<String>,
+    pub result_table_name: Option<String>,
+    pub is_processing: Option<bool>,
+    pub last_run: Option<String>,
+    pub logs: Option<Vec<ProcessLog>>
+}
+
+impl Default for ProcessResponse {
+    fn default() -> Self {
+        ProcessResponse {
+            id: None,
+            is_error: None,
+            summary: None,
+            result_table_name: None,
+            is_processing: None,
+            last_run: None,
+            logs: None
+        }
+    }
+}
+
+impl ProcessResponse {
+    pub fn from_process_response(source: RunBusinessProcessResponse) -> Self {
+        let mut process_response = ProcessResponse::default();
+        process_response.id = Some(source.id);
+        process_response.is_error = Some(source.is_error);
+        process_response.summary = Some(source.summary);
+        process_response.result_table_name = Some(source.result_table_name);
+        process_response.is_processing = Some(source.is_processing);
+        process_response.last_run = Some(source.last_run);
+        process_response.logs = Some(source.logs.iter().map(|log_value| {
+            ProcessLog {
+                record_id: Some(log_value.to_owned().record_id),
+                log: Some(log_value.to_owned().log)
+            }
+        }).collect());
+        process_response
+    }
+}
+
 #[derive(Deserialize, Extractible, Debug, Clone)]
 pub struct KeyAndValue {
     pub key: String,
     pub int_value: Option<i32>,
     pub boolean_value: Option<bool>,
     pub string_value: Option<String>,
+    pub date_value: Option<String>,
     pub decimal_value: Option<f64>,
     pub value_type: Option<String> 
 }
@@ -42,30 +93,32 @@ impl KeyAndValue {
         let mut value = Value {
             ..Default::default()
         };
-        match self.value_type {
-            Some(value_type) => {
-                if value_type == "INTEGER" {
-                    value.int_value = self.int_value.unwrap_or_default();
-                    value.value_type = ValueType::Integer.to_owned().into();
-                } else if value_type == "DECIMAL" {
-
-                } else if value_type == "BOOLEAN" {
-                    value.boolean_value = self.boolean_value.unwrap_or_default();
-                    value.value_type = ValueType::Boolean.to_owned().into();
-                } else if value_type == "STRING" {
-                    value.string_value = self.string_value.unwrap_or_default();
-                    value.value_type = ValueType::String.to_owned().into();
-                } else if value_type == "DATE" {
-                    value.string_value = self.string_value.unwrap_or_default();
-                    value.value_type = ValueType::Date.to_owned().into();
-                }
-            },
-            None => {}
-        };
+        if self.int_value.is_some() {
+            value.int_value = self.int_value.unwrap_or_default();
+            value.value_type = ValueType::Integer.to_owned().into();
+        } else if self.decimal_value.is_some() {
+            let numeric_value = self.decimal_value.unwrap();
+            let string_value = numeric_value.to_string();
+            let decimal_index = string_value.find(".");
+            let precision = (string_value.len() - 1) - decimal_index.unwrap_or_default();
+            value.decimal_value = Some(Decimal {
+                decimal_value: numeric_value.to_string(),
+                scale: precision as i32
+            });
+            value.value_type = ValueType::Decimal.to_owned().into();
+        } else if self.boolean_value.is_some() {
+            value.boolean_value = self.boolean_value.unwrap_or_default();
+            value.value_type = ValueType::Boolean.to_owned().into();
+        } else if self.string_value.is_some() {
+            value.string_value = self.string_value.unwrap_or_default();
+            value.value_type = ValueType::String.to_owned().into();
+        } else if self.date_value.is_some() {
+            value.date_value = self.date_value.unwrap_or_default();
+            value.value_type = ValueType::Date.to_owned().into();
+        }
         KeyValue {
             key: self.key, 
             value: Some(value) 
-        
         }
     }
 }
@@ -80,4 +133,44 @@ pub struct EntityNew {
 #[extract(default_source(from = "body", format = "json"))]
 pub struct EntityNewDocument {
     pub entity: Option<EntityNew>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+pub struct EntityUpdate {
+    pub table_name: Option<String>,
+    pub id: Option<i32>,
+    pub attributes: Option<Vec<KeyAndValue>>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+#[extract(default_source(from = "body", format = "json"))]
+pub struct EntityUpdateDocument {
+    pub entity: Option<EntityUpdate>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+pub struct EntityDelete {
+    pub table_name: Option<String>,
+    pub id: Option<i32>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+#[extract(default_source(from = "body", format = "json"))]
+pub struct EntityDeleteDocument {
+    pub entity: Option<EntityDelete>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+pub struct RunProcess {
+    pub table_name: Option<String>,
+    pub id: Option<i32>,
+    pub process_code: Option<String>,
+    pub table_selected_id: Option<i32>,
+    pub parameters: Option<Vec<KeyAndValue>>
+}
+
+#[derive(Deserialize, Extractible, Debug, Clone)]
+#[extract(default_source(from = "body", format = "json"))]
+pub struct RunProcessDocument {
+    pub process: Option<RunProcess>
 }
